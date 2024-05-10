62e332619aca65ba49a4d1980c9ebed3ee249ce5
max <cao.4321519@163.com>
2024-05-10 14:14:47 +0800

    init

---
+++ src/components/tx-flow/flows/SuccessScreen/index.tsx
@@ -0,0 +1,109 @@
+import StatusStepper from './StatusStepper'
+import { Button, Container, Divider, Paper } from '@mui/material'
+import classnames from 'classnames'
+import Link from 'next/link'
+import css from './styles.module.css'
+import { useAppSelector } from '@/store'
+import { PendingStatus, selectPendingTxById } from '@/store/pendingTxsSlice'
+import { useCallback, useContext, useEffect, useState } from 'react'
+import { getTxLink } from '@/hooks/useTxNotifications'
+import { useCurrentChain } from '@/hooks/useChains'
+import { TxEvent, txSubscribe } from '@/services/tx/txEvents'
+import useSafeInfo from '@/hooks/useSafeInfo'
+import { TxModalContext } from '../..'
+import LoadingSpinner, { SpinnerStatus } from '@/components/new-safe/create/steps/StatusStep/LoadingSpinner'
+import { ProcessingStatus } from '@/components/tx-flow/flows/SuccessScreen/statuses/ProcessingStatus'
+import { IndexingStatus } from '@/components/tx-flow/flows/SuccessScreen/statuses/IndexingStatus'
+import { DefaultStatus } from '@/components/tx-flow/flows/SuccessScreen/statuses/DefaultStatus'
+
+const SuccessScreen = ({ txId }: { txId: string }) => {
+  const [localTxHash, setLocalTxHash] = useState<string>()
+  const [error, setError] = useState<Error>()
+  const { setTxFlow } = useContext(TxModalContext)
+  const chain = useCurrentChain()
+  const pendingTx = useAppSelector((state) => selectPendingTxById(state, txId))
+  const { safeAddress } = useSafeInfo()
+  const { status } = pendingTx || {}
+  const txHash = pendingTx && 'txHash' in pendingTx ? pendingTx.txHash : undefined
+  const txLink = chain && getTxLink(txId, chain, safeAddress)
+
+  useEffect(() => {
+    if (!txHash) return
+
+    setLocalTxHash(txHash)
+  }, [txHash])
+
+  useEffect(() => {
+    const unsubFns: Array<() => void> = ([TxEvent.FAILED, TxEvent.REVERTED] as const).map((event) =>
+      txSubscribe(event, (detail) => {
+        if (detail.txId === txId && pendingTx) setError(detail.error)
+      }),
+    )
+
+    return () => unsubFns.forEach((unsubscribe) => unsubscribe())
+  }, [txId, pendingTx])
+
+  const onClose = useCallback(() => {
+    setTxFlow(undefined)
+  }, [setTxFlow])
+
+  const isSuccess = status === undefined
+  const spinnerStatus = error ? SpinnerStatus.ERROR : isSuccess ? SpinnerStatus.SUCCESS : SpinnerStatus.PROCESSING
+
+  let StatusComponent
+  switch (status) {
+    case PendingStatus.PROCESSING:
+    case PendingStatus.RELAYING:
+      StatusComponent = <ProcessingStatus txId={txId} pendingTx={pendingTx} />
+      break
+    case PendingStatus.INDEXING:
+      StatusComponent = <IndexingStatus />
+      break
+    default:
+      StatusComponent = <DefaultStatus error={error} />
+  }
+
+  return (
+    <Container
+      component={Paper}
+      disableGutters
+      sx={{
+        textAlign: 'center',
+        maxWidth: `${900 - 75}px`, // md={11}
+      }}
+      maxWidth={false}
+    >
+      <div className={css.row}>
+        <LoadingSpinner status={spinnerStatus} />
+        {StatusComponent}
+      </div>
+
+      {!error && (
+        <>
+          <Divider />
+          <div className={css.row}>
+            <StatusStepper status={status} txHash={localTxHash} />
+          </div>
+        </>
+      )}
+
+      <Divider />
+
+      <div className={classnames(css.row, css.buttons)}>
+        {txLink && (
+          <Link {...txLink} passHref target="_blank" rel="noreferrer" legacyBehavior>
+            <Button data-testid="view-transaction-btn" variant="outlined" size="small" onClick={onClose}>
+              View transaction
+            </Button>
+          </Link>
+        )}
+
+        <Button data-testid="finish-transaction-btn" variant="contained" size="small" onClick={onClose}>
+          Finish
+        </Button>
+      </div>
+    </Container>
+  )
+}
+
+export default SuccessScreen
---
+++ src/services/tx/__tests__/txMonitor.test.ts
@@ -0,0 +1,397 @@
+import { _getRemainingTimeout } from '@/services/tx/txMonitor'
+import * as txEvents from '@/services/tx/txEvents'
+import * as txMonitor from '@/services/tx/txMonitor'
+
+import { act } from '@testing-library/react'
+import { SafeCreationStatus } from '@/components/new-safe/create/steps/StatusStep/useSafeCreation'
+import { toBeHex } from 'ethers'
+import { MockEip1193Provider } from '@/tests/mocks/providers'
+import { BrowserProvider, type JsonRpcProvider, type TransactionReceipt } from 'ethers'
+import { faker } from '@faker-js/faker'
+import { SimpleTxWatcher } from '@/utils/SimpleTxWatcher'
+
+const { waitForTx, waitForRelayedTx, waitForCreateSafeTx } = txMonitor
+
+const provider = new BrowserProvider(MockEip1193Provider) as unknown as JsonRpcProvider
+
+const setupFetchStub = (data: any) => (_url: string) => {
+  return Promise.resolve({
+    json: () => Promise.resolve(data),
+    status: 200,
+    ok: true,
+  })
+}
+
+describe('txMonitor', () => {
+  const simpleTxWatcherInstance = SimpleTxWatcher.getInstance()
+
+  let txDispatchSpy = jest.spyOn(txEvents, 'txDispatch')
+  let waitForTxSpy = jest.spyOn(provider, 'waitForTransaction')
+  // let simpleWatcherSpy = jest.spyOn(SimpleTxWatcher, 'getInstance')
+  const safeAddress = toBeHex('0x123', 20)
+
+  let watchTxHashSpy = jest.spyOn(simpleTxWatcherInstance, 'watchTxHash')
+
+  beforeEach(() => {
+    jest.useFakeTimers()
+    jest.resetAllMocks()
+
+    txDispatchSpy = jest.spyOn(txEvents, 'txDispatch')
+    waitForTxSpy = jest.spyOn(provider, 'waitForTransaction')
+    watchTxHashSpy = jest.spyOn(simpleTxWatcherInstance, 'watchTxHash')
+  })
+
+  describe('waitForTx', () => {
+    // Not mined/validated:
+    it("emits a FAILED event if waitForTransaction isn't blocking and no receipt was returned", async () => {
+      // Can return null if waitForTransaction is non-blocking:
+      // https://docs.ethers.io/v5/single-page/#/v5/api/providers/provider/-%23-Provider-waitForTransaction
+      const receipt = null as unknown as TransactionReceipt
+      watchTxHashSpy.mockImplementation(() => Promise.resolve(receipt))
+      await waitForTx(provider, ['0x0'], '0x0', safeAddress, faker.finance.ethereumAddress(), 1)
+
+      expect(txDispatchSpy).toHaveBeenCalledWith('FAILED', { txId: '0x0', error: expect.any(Error) })
+    })
+
+    it('emits a REVERTED event if the tx reverted', async () => {
+      const receipt = {
+        status: 0,
+      } as TransactionReceipt
+
+      watchTxHashSpy.mockImplementation(() => Promise.resolve(receipt))
+      await waitForTx(provider, ['0x0'], '0x0', safeAddress, faker.finance.ethereumAddress(), 1)
+
+      expect(txDispatchSpy).toHaveBeenCalledWith('REVERTED', {
+        txId: '0x0',
+        error: new Error('Transaction reverted by EVM.'),
+      })
+    })
+
+    it('emits a FAILED event if waitForTransaction throws', async () => {
+      watchTxHashSpy.mockImplementation(() => Promise.reject(new Error('Test error.')))
+      await waitForTx(provider, ['0x0'], '0x0', safeAddress, faker.finance.ethereumAddress(), 1)
+
+      expect(txDispatchSpy).toHaveBeenCalledWith('FAILED', { txId: '0x0', error: new Error('Test error.') })
+    })
+  })
+
+  describe('waitForRelayedTx', () => {
+    it("emits a PROCESSED event if taskStatus 'ExecSuccess'", async () => {
+      const mockData = {
+        task: {
+          taskState: 'ExecSuccess',
+        },
+      }
+      global.fetch = jest.fn().mockImplementation(setupFetchStub(mockData))
+
+      const mockFetch = jest.spyOn(global, 'fetch')
+
+      waitForRelayedTx('0x1', ['0x2'], safeAddress)
+
+      await act(() => {
+        jest.advanceTimersByTime(15_000 + 1)
+      })
+
+      expect(mockFetch).toHaveBeenCalledTimes(1)
+      expect(txDispatchSpy).toHaveBeenCalledWith('PROCESSED', { txId: '0x2', safeAddress })
+
+      // The relay timeout should have been cancelled
+      txDispatchSpy.mockClear()
+      await act(() => {
+        jest.advanceTimersByTime(3 * 60_000 + 1)
+      })
+      expect(txDispatchSpy).not.toHaveBeenCalled()
+    })
+
+    it("emits a REVERTED event if taskStatus 'ExecReverted'", async () => {
+      const mockData = {
+        task: {
+          taskState: 'ExecReverted',
+        },
+      }
+      global.fetch = jest.fn().mockImplementation(setupFetchStub(mockData))
+
+      const mockFetch = jest.spyOn(global, 'fetch')
+
+      waitForRelayedTx('0x1', ['0x2'], safeAddress)
+
+      await act(() => {
+        jest.advanceTimersByTime(15_000 + 1)
+      })
+
+      expect(mockFetch).toHaveBeenCalledTimes(1)
+      expect(txDispatchSpy).toHaveBeenCalledWith('REVERTED', {
+        txId: '0x2',
+        error: new Error(`Relayed transaction reverted by EVM.`),
+      })
+
+      // The relay timeout should have been cancelled
+      txDispatchSpy.mockClear()
+      await act(() => {
+        jest.advanceTimersByTime(3 * 60_000 + 1)
+      })
+      expect(txDispatchSpy).not.toHaveBeenCalled()
+    })
+
+    it("emits a FAILED event if taskStatus 'Blacklisted'", async () => {
+      const mockData = {
+        task: {
+          taskState: 'Blacklisted',
+        },
+      }
+      global.fetch = jest.fn().mockImplementation(setupFetchStub(mockData))
+
+      const mockFetch = jest.spyOn(global, 'fetch')
+
+      waitForRelayedTx('0x1', ['0x2'], safeAddress)
+
+      await act(() => {
+        jest.advanceTimersByTime(15_000 + 1)
+      })
+
+      expect(mockFetch).toHaveBeenCalledTimes(1)
+      expect(txDispatchSpy).toHaveBeenCalledWith('FAILED', {
+        txId: '0x2',
+        error: new Error(`Relayed transaction was blacklisted by relay provider.`),
+      })
+
+      // The relay timeout should have been cancelled
+      txDispatchSpy.mockClear()
+      await act(() => {
+        jest.advanceTimersByTime(3 * 60_000 + 1)
+      })
+      expect(txDispatchSpy).not.toHaveBeenCalled()
+    })
+
+    it("emits a FAILED event if taskStatus 'Cancelled'", async () => {
+      const mockData = {
+        task: {
+          taskState: 'Cancelled',
+        },
+      }
+      global.fetch = jest.fn().mockImplementation(setupFetchStub(mockData))
+
+      const mockFetch = jest.spyOn(global, 'fetch')
+
+      waitForRelayedTx('0x1', ['0x2'], safeAddress)
+
+      await act(() => {
+        jest.advanceTimersByTime(15_000 + 1)
+      })
+
+      expect(mockFetch).toHaveBeenCalledTimes(1)
+      expect(txDispatchSpy).toHaveBeenCalledWith('FAILED', {
+        txId: '0x2',
+        error: new Error(`Relayed transaction was cancelled by relay provider.`),
+      })
+
+      // The relay timeout should have been cancelled
+      txDispatchSpy.mockClear()
+      await act(() => {
+        jest.advanceTimersByTime(3 * 60_000 + 1)
+      })
+      expect(txDispatchSpy).not.toHaveBeenCalled()
+    })
+
+    it("emits a FAILED event if taskStatus 'NotFound'", async () => {
+      const mockData = {
+        task: {
+          taskState: 'NotFound',
+        },
+      }
+      global.fetch = jest.fn().mockImplementation(setupFetchStub(mockData))
+
+      const mockFetch = jest.spyOn(global, 'fetch')
+
+      waitForRelayedTx('0x1', ['0x2'], safeAddress)
+
+      await act(() => {
+        jest.advanceTimersByTime(15_000 + 1)
+      })
+
+      expect(mockFetch).toHaveBeenCalledTimes(1)
+      expect(txDispatchSpy).toHaveBeenCalledWith('FAILED', {
+        txId: '0x2',
+        error: new Error(`Relayed transaction was not found.`),
+      })
+
+      // The relay timeout should have been cancelled
+      txDispatchSpy.mockClear()
+      await act(() => {
+        jest.advanceTimersByTime(3 * 60_000 + 1)
+      })
+      expect(txDispatchSpy).not.toHaveBeenCalled()
+    })
+
+    it('emits a FAILED event if the tx relaying timed out', async () => {
+      const mockData = {
+        task: {
+          taskState: 'WaitingForConfirmation',
+        },
+      }
+      global.fetch = jest.fn().mockImplementation(setupFetchStub(mockData))
+
+      waitForRelayedTx('0x1', ['0x2'], safeAddress)
+
+      await act(() => {
+        jest.advanceTimersByTime(3 * 60_000 + 1)
+      })
+
+      expect(txDispatchSpy).toHaveBeenCalledWith('FAILED', {
+        txId: '0x2',
+        error: new Error('Transaction not relayed in 3 minutes. Be aware that it might still be relayed.'),
+      })
+    })
+  })
+
+  describe('waitForCreateSafeTx', () => {
+    it("sets the status to SUCCESS if taskStatus 'ExecSuccess'", async () => {
+      const mockData = {
+        task: {
+          taskState: 'ExecSuccess',
+        },
+      }
+      global.fetch = jest.fn().mockImplementation(setupFetchStub(mockData))
+
+      const mockFetch = jest.spyOn(global, 'fetch')
+      const setStatusSpy = jest.fn()
+
+      waitForCreateSafeTx('0x1', setStatusSpy)
+
+      await act(() => {
+        jest.advanceTimersByTime(15_000 + 1)
+      })
+
+      expect(mockFetch).toHaveBeenCalledTimes(1)
+      expect(setStatusSpy).toHaveBeenCalledWith(SafeCreationStatus.SUCCESS)
+    })
+
+    it("sets the status to ERROR if taskStatus 'ExecReverted'", async () => {
+      const mockData = {
+        task: {
+          taskState: 'ExecReverted',
+        },
+      }
+      global.fetch = jest.fn().mockImplementation(setupFetchStub(mockData))
+
+      const mockFetch = jest.spyOn(global, 'fetch')
+      const setStatusSpy = jest.fn()
+
+      waitForCreateSafeTx('0x1', setStatusSpy)
+
+      await act(() => {
+        jest.advanceTimersByTime(15_000 + 1)
+      })
+
+      expect(mockFetch).toHaveBeenCalledTimes(1)
+      expect(setStatusSpy).toHaveBeenCalledWith(SafeCreationStatus.ERROR)
+    })
+
+    it("sets the status to ERROR if taskStatus 'Blacklisted'", async () => {
+      const mockData = {
+        task: {
+          taskState: 'Blacklisted',
+        },
+      }
+      global.fetch = jest.fn().mockImplementation(setupFetchStub(mockData))
+
+      const mockFetch = jest.spyOn(global, 'fetch')
+      const setStatusSpy = jest.fn()
+
+      waitForCreateSafeTx('0x1', setStatusSpy)
+
+      await act(() => {
+        jest.advanceTimersByTime(15_000 + 1)
+      })
+
+      expect(mockFetch).toHaveBeenCalledTimes(1)
+      expect(setStatusSpy).toHaveBeenCalledWith(SafeCreationStatus.ERROR)
+    })
+
+    it("sets the status to ERROR if taskStatus 'Cancelled'", async () => {
+      const mockData = {
+        task: {
+          taskState: 'Cancelled',
+        },
+      }
+      global.fetch = jest.fn().mockImplementation(setupFetchStub(mockData))
+
+      const mockFetch = jest.spyOn(global, 'fetch')
+      const setStatusSpy = jest.fn()
+
+      waitForCreateSafeTx('0x1', setStatusSpy)
+
+      await act(() => {
+        jest.advanceTimersByTime(15_000 + 1)
+      })
+
+      expect(mockFetch).toHaveBeenCalledTimes(1)
+      expect(setStatusSpy).toHaveBeenCalledWith(SafeCreationStatus.ERROR)
+    })
+
+    it("sets the status to ERROR if taskStatus 'NotFound'", async () => {
+      const mockData = {
+        task: {
+          taskState: 'NotFound',
+        },
+      }
+      global.fetch = jest.fn().mockImplementation(setupFetchStub(mockData))
+
+      const mockFetch = jest.spyOn(global, 'fetch')
+      const setStatusSpy = jest.fn()
+
+      waitForCreateSafeTx('0x1', setStatusSpy)
+
+      await act(() => {
+        jest.advanceTimersByTime(15_000 + 1)
+      })
+
+      expect(mockFetch).toHaveBeenCalledTimes(1)
+      expect(setStatusSpy).toHaveBeenCalledWith(SafeCreationStatus.ERROR)
+    })
+
+    it('sets the status to ERROR if the tx relaying timed out', async () => {
+      const mockData = {
+        task: {
+          taskState: 'WaitingForConfirmation',
+        },
+      }
+      global.fetch = jest.fn().mockImplementation(setupFetchStub(mockData))
+
+      const mockFetch = jest.spyOn(global, 'fetch')
+      const setStatusSpy = jest.fn()
+
+      waitForCreateSafeTx('0x1', setStatusSpy)
+
+      await act(() => {
+        jest.advanceTimersByTime(3 * 60_000 + 1)
+      })
+
+      expect(mockFetch).toHaveBeenCalled()
+      expect(setStatusSpy).toHaveBeenCalledWith(SafeCreationStatus.ERROR)
+    })
+  })
+})
+
+describe('getRemainingTimeout', () => {
+  const DefaultTimeout = 1
+
+  it('returns 1 if submission is older than 1 minute', () => {
+    const result = _getRemainingTimeout(DefaultTimeout, Date.now() - DefaultTimeout * 60_000)
+
+    expect(result).toBe(1)
+  })
+
+  it('returns default timeout in milliseconds if no submission time was passed', () => {
+    const result = _getRemainingTimeout(DefaultTimeout)
+
+    expect(result).toBe(DefaultTimeout * 60_000)
+  })
+
+  it('returns remaining timeout', () => {
+    const passedMinutes = DefaultTimeout - 0.4
+    const result = _getRemainingTimeout(DefaultTimeout, Date.now() - passedMinutes * 60_000)
+
+    expect(result).toBe((DefaultTimeout - passedMinutes) * 60_000)
+  })
+})
